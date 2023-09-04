eba94465857bfbfabd19a2df11f9cff7938ae831
Gab <gabcam071118.rg@gmail.com>
2023-09-04 12:44:13 -0600

    Modificacion de errores archivo principal.php

--- practicas/p01/inclusion_archivos/pie.inc.php
+++ practicas/p01/inclusion_archivos/pie.inc.php
@@ -1,4 +1,10 @@
+<hr/>
 <?php
-echo "<h1> Este es el cuerpo del documento </h1>"; 
-echo "<h2> Este es el cuerpo del documento </h2>";
+echo "<div><h1 style=\"border-width:3;border-style:groove; background-color:
+#ffcc99;\"> Final de la página PHP Vínculos útiles : <a href=\"php.net\">php.net</a>
+&nbsp; <a href=\"mysql.org\">mysql.org</a></h1>"; 
+echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp; &nbsp;";
+echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
 ?>
+</body>
+</html>
--- practicas/p01/inclusion_archivos/principal.php
+++ practicas/p01/inclusion_archivos/principal.php
@@ -1,19 +1,7 @@
-<body>
-<script language="php"> 
-echo "<h1> BUENO DIAS A TODOS </h1>";
-</script>
-<?php 
-echo "<h2> Titulo escrito por PHP </h2>";
-$variable2="MySQL";
+<?php
+include("encabezado.inc.php"); 
+echo "<hr/>";
+include_once("cuerpo.inc.php"); 
+require("cuerpo.html"); 
+require_once("pie.inc.php"); 
 ?>
-<p>Vas a descubrir <?= $variable1 ?></p>
-<?php 
-echo "<h2> Buenos días de $variable1 </h2>";
-?>
-<p> Utilización de variables PHP <br/> Vas a descubrir igualmente
-<?php 
-echo $variable2;
-?>
-</p>
-<?= "<div><big> Buenos días de $variable2 </big></div>" ?> 
-</body>
