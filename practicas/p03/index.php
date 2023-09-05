7eb6a7bdc423f5518eabda4f1b1a2a9cddfa8343
gab <gabcam071118.rg@gmail.com>
2023-09-05 12:51:11 -0600

    Ejercicio 2

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -18,13 +18,46 @@
     $_element1; 
     //$house*5;       Invalida 
     echo '<ul>';
-    echo  '<li> <b>_myvar</b> es válida porque inicia con guión bajo </li>';
-    echo  '<li> <b>_7var</b> es válida porque inicia con guión bajo </li>';
-    echo  '<li> <b>myvar</b> es válida porque inicia con una letra</li>';
-    echo  '<li> <b>var7</b> es válida porque inicia con una letra </li>';
-    echo  '<li> <b>_element1</b> es válida porque inicia con guión bajo </li>';
+        echo  '<li> <b>_myvar</b> es válida porque inicia con guión bajo </li>';
+        echo  '<li> <b>_7var</b> es válida porque inicia con guión bajo </li>';
+        echo  '<li> <b>myvar</b> es válida porque inicia con una letra</li>';
+        echo  '<li> <b>var7</b> es válida porque inicia con una letra </li>';
+        echo  '<li> <b>_element1</b> es válida porque inicia con guión bajo </li>';
     echo '</ul>';
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <ul>
+        <li>$a = “ManejadorSQL”;</li>
+        <li>$b = 'MySQL’;</li>
+        <li>$c = &$a;</li>
+    </ul>
+    <?php
+    $a = 'ManejadorSQL';
+    $b = 'MySQL';
+    $c = &$a;
+    echo 'Ahora muestra el contenido de cada variable';
+    echo '<ul>';
+        echo '<li>', $a ,'</li>';
+        echo '<li>', $b ,'</li>';
+        echo '<li>', $c ,'</li>';
+    echo '</ul>';
+    echo 'Agrega al código actual las siguientes asignaciones:';
+    echo '<ul>';
+        echo '<li> $a = “PHP server”;  </li>';
+        echo '<li> $b = &$a; </li>';
+    echo '</ul>';
+    $a = 'PHP server';  
+    $b = &$a;
+    echo 'Vuelve a mostrar el contenido de cada uno: ';
+    echo '<ul>';
+        echo '<li>', $a ,'</li>';
+        echo '<li>', $b ,'</li>';
+    echo '</ul>';
+    echo 'Se mostró la nueva asignación de las variables, la cúal la variable $b se le asignó $a';
+    ?>
+
+
 
 
 
