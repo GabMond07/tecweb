ae374c3779b9849857a6bff379ad53d3fa54d4f7
gab <gabcam071118.rg@gmail.com>
2023-09-17 20:15:25 -0600

    Función para generar números aleatorios hasta obtener una secuencia deseada

--- practicas/p03/p03_funciones.php
+++ practicas/p03/p03_funciones.php
@@ -7,4 +7,28 @@ function esMultiploDe5Y7($numero) {
         return false;
     }
 }
+
+
+// Función para generar números aleatorios hasta obtener una secuencia deseada
+function generarSecuencia($secuencia_deseada) {
+    $matriz = array();
+    $iteraciones = 0;
+
+    while (true) {
+        $numeros = array();
+
+        for ($i = 0; $i < 3; $i++) {
+            $numero = rand(1, 1000);
+            $numeros[] = $numero;
+        }
+
+        if ($numeros == $secuencia_deseada) {
+            return array('matriz' => $matriz, 'iteraciones' => $iteraciones);
+        }
+
+        $matriz[] = $numeros;
+        $iteraciones++;
+    }
+}
+
 ?>
